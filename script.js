c90ba3232791b237f8821cb7731fb20320026abc
Hopeflowersy <berryciaa77@gmail.com>
2025-10-06 14:12:52 +0700

    Pisah CSS, tambahkan font sans-serif dan warna

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-function halo() {
-  alert("Web pertama njir, nisa jawa");
+function showNotif() {
+  alert("Nisa sebenarnya adalah wujud asli musang bendang yang suka mencuri bahkan membunuh orang tidak bersalah, konon katanya kalau akun robloxnya dihapus, sifatnya akan menjadi lebih baik");
 }
